5d9be16fcd0940a02fb854c56ff1cd0139a8aed7
João Casemiro <jv.casemiro@icloud.com>
2022-05-20 15:33:17 -0300

    Inserindo template engine para envio de e-mail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { inject, injectable } from 'tsyringe';
 import { v4 } from 'uuid';
 
@@ -21,6 +22,14 @@ export class SendForgotPasswordMailUseCase {
   ) {}
   async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
+    const templatePath = resolve(
+      __dirname,
+      '..',
+      '..',
+      'views',
+      'emails',
+      'forgotPassword.hbs'
+    );
 
     if (!user) {
       throw new AppError('User does not exists');
@@ -36,10 +45,16 @@ export class SendForgotPasswordMailUseCase {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_EMAIL_URL}${token}`,
+    };
+
     await this.mailProvider.sendMail(
       email,
       'Recuperação de senha',
-      `O link para o reset é ${token}`
+      variables,
+      templatePath
     );
   }
 }
